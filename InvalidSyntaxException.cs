7bcf4bf7b614fa38f7d3288d900f32715e0e63e5
Andrey <andrey2002335@gmail.com>
2020-06-08 11:10:19 +0300

    One more test

--- InvalidSyntaxException.cs
+++ InvalidSyntaxException.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Interpretator
 {
-    class InvalidSyntaxException
+    class InvalidSyntaxException:Exception
     {
+        public InvalidSyntaxException()
+        {
+        }
+
+        public InvalidSyntaxException(string message) : base(message)
+        {
+        }
+
+        public InvalidSyntaxException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidSyntaxException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -21,11 +21,19 @@ namespace Interpretator
             string result = strArr[count];
             strArr.RemoveAt(count);
             System.Console.WriteLine(result);
-
+            strArr.RemoveAt(count);
+            for (int i = 0; i < strArr.Count; i++)
+            {
+                string[] str = strArr[i].Split(" = ");
+                result = result.Replace(str[0], str[1]);
+            }
             string x  = System.Console.ReadLine();
             Interpretator.Parser interpreter = new Interpretator.Parser(result);
             Interpretator.Expression node = interpreter.Parse();
-
+            using (StreamWriter sw = new StreamWriter("result.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(node.Accept(new Interpretator.ValueBuilder()));
+            }
             Console.WriteLine($"Ответ: {node.Accept(new Interpretator.ValueBuilder())}");
             Console.WriteLine("Hello World!");
         }
