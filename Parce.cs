cfeb657daa33923b026ddc0dd8f6f3eb494389f0
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-08 08:57:34 +0300

    add operations

---
+++ Parce.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpretator
+{
+ public   class Parce
+    {
+
+        public enum Actions
+        {
+            Plus,
+                Minus,
+                Multiply,
+                Divide,
+                Number, None, LeftParenthesis, RightParenthesis
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.IO;
 
 namespace Interpretator
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {//entrance file
+            StreamReader fs = new StreamReader("Input.txt");
+            
+            
+            
+            
             Console.WriteLine("Hello World!");
         }
     }
