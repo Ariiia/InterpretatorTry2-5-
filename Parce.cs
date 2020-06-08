17acd26455830f8e1f587c3b25924ac72e862928
Dariia Kariavka <koalaansthen@gmail.com>
2020-06-08 10:11:39 +0300

    add token contr

--- Parce.cs
+++ Parce.cs
@@ -4,9 +4,22 @@ using System.Text;
 
 namespace Interpretator
 {
- public   class Parce
+ public   class Parse
     {
+        private Token currentToken;
+        private int currentPos;
+        private int charCount;
+        private char currentChar;
+        public string Text { get; private set; }
+        public Parse(string text)
+        {
+            this.Text = string.IsNullOrEmpty(text) ? string.Empty : text;
+            this.charCount = this.Text.Length;
+            this.currentToken = Token.None();
 
+            this.currentPos = -1;
+            this.Advance();
+        }
         public enum Actions
         {
             Plus,
